65b013e581746bb58b42373c840e0d12deecd911
chaitanya potti <chaitanya.potti@gmail.com>
2018-10-10 11:42:32 +0530

    temp login screen

--- src/pages/LoginScreen/index.js
+++ src/pages/LoginScreen/index.js
@@ -1,17 +1,19 @@
 /* eslint camelcase: 0 */
 
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import Login from "./Login";
 
 class LoginScreen extends Component {
   render() {
     return (
       <div className="hr-vr-cntr">
         <div className="hl">
-          <div className="hli"><span className="logo"/></div>
-          <div className="hli crd-mrgn crd-animation"><span className="metamask-card"/></div>
-          <div className="hli crd-mrgn crd-animation"><span className="prvt-key-card"/></div>
-          <div className="hli crd-mrgn crd-animation"><span className="sign-in-card"/></div>
-          <div className="hli crd-mrgn crd-animation"><span className="attach-json-card"/></div>
+          <div className="hli">
+            <span className="logo" />
+          </div>
+          {/* <div className="hli crd-mrgn crd-animation"><span className="metamask-card"/></div>
+          <div className="hli crd-mrgn crd-animation"><span className="prvt-key-card"/></div> */}
+          <Login />
         </div>
       </div>
     );

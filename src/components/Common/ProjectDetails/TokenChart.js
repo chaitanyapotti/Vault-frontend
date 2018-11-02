8896df96df03f263e67bb30e3c3a2cf545c384b4
Vinay Bagul <vinay@yun.buzz>
2018-11-02 14:24:47 +0530

    pie charts 6/7

--- src/components/Common/ProjectDetails/TokenChart.js
+++ src/components/Common/ProjectDetails/TokenChart.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Tooltip, Legend, Pie, PieChart, Cell } from "recharts";
 import { Row, Col } from "../../../helpers/react-flexbox-grid";
 import { formatFromWei } from "../../../helpers/common/projectDetailhelperFunctions";
+import PieChartComponent from "../../../components/Common/PieChartComponent";
 
 const CHARTCOLORS = ["#001d33", "#0f395b", "#1e5583", "#3d8dd4", "#4ca9fc", "#65b6fd", "#7ec3fe", "#b0ddff", "#e1f4ff", "#2e71ac"];
 
@@ -9,17 +10,30 @@ class TokenChart extends Component {
   render() {
     const { rounds, foundationDetails } = this.props || {};
     let i = 0;
+    let totalSaleTokens = 0;
+    
+    for (let index = 0; index < rounds.length; index++) {
+      const element = rounds[index];
+      totalSaleTokens += formatFromWei(element.tokenCount, 0);
+    }
+    for (let index = 0; index < foundationDetails.length; index++) {
+      const element = foundationDetails[index];
+      totalSaleTokens += formatFromWei(element.amount, 0);
+    }
+    
     let interDetails = rounds.map(round => {
       const { tokenCount } = round || 0;
       i += 1;
-      return { value: formatFromWei(tokenCount), description: `Round ${i}` };
+      return { entityPercentage: formatFromWei(tokenCount)/ totalSaleTokens * 100, entityName: `Round ${i}` };
     });
-    interDetails = interDetails.concat(
+    console.log(interDetails)
+    const foundDetails = 
       foundationDetails.map(foundationRequest => {
         const { amount = 0, description = "team" } = foundationRequest;
-        return { value: formatFromWei(amount), description };
+        return { entityPercentage: formatFromWei(amount) / totalSaleTokens * 100, entityName: description };
       })
-    );
+      console.log(foundDetails)
+    
     return (
       <div className="push-top--50">
         <div className="txt-xl text--primary">Token Distribution Chart</div>
@@ -27,9 +41,11 @@ class TokenChart extends Component {
         <Row>
           <Col xs={12} lg={6}>
             <div>
-              <PieChart width={400} height={500}>
+            <PieChartComponent nonSaleEntities={foundDetails} saleEntities={interDetails} totalSaleTokens={totalSaleTokens}/>
+              {/* <PieChart width={400} height={500}>
                 <Legend />
                 <Tooltip />
+                
                 <Pie
                   isAnimationActive
                   data={interDetails}
@@ -46,7 +62,7 @@ class TokenChart extends Component {
                     <Cell key={Math.random()} fill={CHARTCOLORS[index % CHARTCOLORS.length]} />
                   ))}
                 </Pie>
-              </PieChart>
+              </PieChart> */}
             </div>
           </Col>
         </Row>

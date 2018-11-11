bf43cece2d7c744026a7bb88202a5cb5ea8eb189
unknown <kvppradeep999@gmail.com>
2018-11-11 13:28:09 +0530

    validations

--- src/pages/WhiteList/index.js
+++ src/pages/WhiteList/index.js
@@ -1,113 +1,167 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import {Grid } from "../../helpers/react-flexbox-grid";
+import { Grid } from "../../helpers/react-flexbox-grid";
 import CustomizedStepper from "../../components/Common/CustomizedStepper";
-import {ButtonComponent} from "../../components/Common/FormComponents";
+import { ButtonComponent } from "../../components/Common/FormComponents";
 import { CUICard, CUIDivider } from "../../helpers/material-ui";
-import {Introduction, EthWallet, TC, BuyersInformation, UploadDocuments, Submit, OtpVerification} from "../../components/Whitelist";
-import { fetchUserFormStates, backButtonAction, nextButtonAction, saveUserFormStates } from "../../actions/userRegistrationActions"
+import { Introduction, EthWallet, TC, BuyersInformation, UploadDocuments, Submit, OtpVerification } from "../../components/Whitelist";
+import { fetchUserFormStates, backButtonAction, nextButtonAction, saveUserFormStates } from "../../actions/userRegistrationActions";
 
 class WhiteList extends Component {
-
-    componentDidMount(){
-        if (this.props.userLocalPublicAddress) {
-            this.props.fetchUserFormStates(this.props.userLocalPublicAddress);
-          } else {
-            this.props.fetchUserFormStates("0xb758c38326Df3D75F1cf0DA14Bb8220Ca4231e74");
-          }
+  componentDidMount() {
+    if (this.props.userLocalPublicAddress) {
+      this.props.fetchUserFormStates(this.props.userLocalPublicAddress);
+    } else {
+      this.props.fetchUserFormStates("0xb758c38326Df3D75F1cf0DA14Bb8220Ca4231e74");
     }
+  }
 
-    getSteps = () => [
-        "Introducton",
-        "ETH Wallet",
-        "Term & Conditions",
-        "Mobile Number Verification",
-        "Buyers Information",
-        "Upload Documents",
-        "Submit"
-    ];
-    
-    getStepContent = () => {
-        switch (this.props.activeStep) {
-            case 0:
-            return <div className="wht-lst-info-cnt"><Introduction /></div>
-            case 1:
-            return <div className="wht-lst-info-cnt"><EthWallet /></div>
-            case 2:
-            return <div className="wht-lst-info-cnt"><TC /></div>
-            case 3:
-            return <div className="wht-lst-info-cnt"><OtpVerification /></div>
-            case 4: 
-            return <div className="wht-lst-info-cnt"><BuyersInformation /></div>
-            case 5: 
-            return <div className="wht-lst-info-cnt"><UploadDocuments /></div>
-            case 6:
-            return <div className="wht-lst-info-cnt"> <Submit/> </div>;
-            default: 
-            return <div>Default ka kuch karo</div>
-        }
-    };
+  getSteps = () => [
+    "Introducton",
+    "ETH Wallet",
+    "Term & Conditions",
+    "Mobile Number Verification",
+    "Buyers Information",
+    "Upload Documents",
+    "Submit"
+  ];
 
-    handleBack = () => {
-        this.props.backButtonAction(this.props.activeStep)
-        // this.setState(state => ({
-        //   activeStep: state.activeStep - 1,
-        // }));
-    };
-
-    handleNext = () => {
-        this.props.nextButtonAction(this.props.activeStep)
-        // this.setState(state => ({
-        //     activeStep: state.activeStep + 1,
-        //   }));
-    };
-
-    handleSave = () => {
-        console.log("user registration data: ", this.props.userRegistrationData)
-        this.props.saveUserFormStates(this.props.userRegistrationData, this.props.userLocalPublicAddress)
+  getDisabledStatus = () => {
+    const { conditionOneAccepted, conditionTwoAccepted, activeStep: getActiveStep } = this.props || {};
+    switch (getActiveStep) {
+      case 0:
+        return false;
+      case 1:
+        return false;
+      case 2:
+        return !conditionOneAccepted || !conditionTwoAccepted;
+      default:
+        return <div>Default ka kuch karo</div>;
     }
+  };
 
-    render() { 
-        return ( 
-            <Grid>
-                <CUICard style={{ padding: "40px 40px", marginBottom: '40px'}}>
-                    <CustomizedStepper getStepContent={this.getStepContent} getSteps={this.getSteps} activeStep={this.props.activeStep} />
-                    <div className="push-top--50"><CUIDivider/></div>
-                    <div className="push--top text--center">
-                        <ButtonComponent label="Back" onClick={this.handleBack} />
-                        <span className="push--left"><ButtonComponent label="Save" onClick={this.handleSave} /></span>
-                        <span className="push--left"><ButtonComponent label="Next" onClick={this.handleNext} /></span>
-                    </div>
-                </CUICard>
-            </Grid> 
+  getStepContent = () => {
+    switch (this.props.activeStep) {
+      case 0:
+        return (
+          <div className="wht-lst-info-cnt">
+            <Introduction />
+          </div>
         );
+      case 1:
+        return (
+          <div className="wht-lst-info-cnt">
+            <EthWallet />
+          </div>
+        );
+      case 2:
+        return (
+          <div className="wht-lst-info-cnt">
+            <TC />
+          </div>
+        );
+      case 3:
+        return (
+          <div className="wht-lst-info-cnt">
+            <OtpVerification />
+          </div>
+        );
+      case 4:
+        return (
+          <div className="wht-lst-info-cnt">
+            <BuyersInformation />
+          </div>
+        );
+      case 5:
+        return (
+          <div className="wht-lst-info-cnt">
+            <UploadDocuments />
+          </div>
+        );
+      case 6:
+        return (
+          <div className="wht-lst-info-cnt">
+            {" "}
+            <Submit />{" "}
+          </div>
+        );
+      default:
+        return <div>Default ka kuch karo</div>;
     }
+  };
+
+  handleBack = () => {
+    this.props.backButtonAction(this.props.activeStep);
+    // this.setState(state => ({
+    //   activeStep: state.activeStep - 1,
+    // }));
+  };
+
+  handleNext = () => {
+    this.props.nextButtonAction(this.props.activeStep);
+    // this.setState(state => ({
+    //     activeStep: state.activeStep + 1,
+    //   }));
+  };
+
+  handleSave = () => {
+    console.log("user registration data: ", this.props.userRegistrationData);
+    this.props.saveUserFormStates(this.props.userRegistrationData, this.props.userLocalPublicAddress);
+  };
+
+  render() {
+    console.log(this.getDisabledStatus(), "logg");
+    return (
+      <Grid>
+        <CUICard style={{ padding: "40px 40px", marginBottom: "40px" }}>
+          <CustomizedStepper getStepContent={this.getStepContent} getSteps={this.getSteps} activeStep={this.props.activeStep} />
+          <div className="push-top--50">
+            <CUIDivider />
+          </div>
+          <div className="push--top text--center">
+            <ButtonComponent label="Back" onClick={this.handleBack} />
+            <span className="push--left">
+              <ButtonComponent label="Save" onClick={this.handleSave} />
+            </span>
+            <span className="push--left">
+              <ButtonComponent label="Next" onClick={this.handleNext} disabled={this.getDisabledStatus()} />
+            </span>
+          </div>
+        </CUICard>
+      </Grid>
+    );
+  }
 }
- 
+
 const mapStateToProps = state => {
-    const { userRegistrationData } = state || {}
-    const { userLocalPublicAddress } = state.signinManagerData || {};
-    const { activeStep } = state.userRegistrationData || {}
-    return {
-        userLocalPublicAddress,
-        activeStep,
-        userRegistrationData
-    };
+  const { userRegistrationData } = state || {};
+  const { userLocalPublicAddress } = state.signinManagerData || {};
+  const { activeStep, conditionOneAccepted, conditionTwoAccepted, phoneNumber, countryCode, otpFromUser } = state.userRegistrationData || {};
+  return {
+    userLocalPublicAddress,
+    activeStep,
+    userRegistrationData,
+    conditionOneAccepted,
+    conditionTwoAccepted,
+    phoneNumber,
+    countryCode,
+    otpFromUser
+  };
 };
 
 const mapDispatchToProps = dispatch =>
-    bindActionCreators(
-        {
-            fetchUserFormStates,
-            backButtonAction,
-            nextButtonAction,
-            saveUserFormStates
-        },
-        dispatch
-    );
+  bindActionCreators(
+    {
+      fetchUserFormStates,
+      backButtonAction,
+      nextButtonAction,
+      saveUserFormStates
+    },
+    dispatch
+  );
 
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(WhiteList);

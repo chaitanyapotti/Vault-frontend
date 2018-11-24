8b3a9c42657b92d13e60eb16a2386cea10271180
chaitanya potti <chaitanya.potti@gmail.com>
2018-11-24 17:57:52 +0530

    loader

--- src/components/Loaders/BtnLoader.js
+++ src/components/Loaders/BtnLoader.js
@@ -4,7 +4,7 @@ import ContentLoader from "react-content-loader";
 const BtnLoader = props => {
   const { height = 19, width = 30 } = props;
   return (
-    <ContentLoader height={1} width={300} speed={2} primaryColor="#f3f3f3" secondaryColor="#ecebeb" {...props}>
+    <ContentLoader height={height} width={width} speed={2} primaryColor="#f3f3f3" secondaryColor="#ecebeb" {...props}>
       <rect x="0" y="0" rx="3" ry="3" width={40} height={8} />
     </ContentLoader>
   );

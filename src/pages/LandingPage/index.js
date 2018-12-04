fc8718a9077865dfb49d08e5973c266395b1ad92
aayush2252 <aayushgupta1991@gmail.com>
2018-12-04 23:42:15 +0530

    landing page css issue resolved

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -15,41 +15,38 @@ class LandingPage extends Component {
     return (
       <Grid>
         <div style={{ marginBottom: "20px" }}>
-          <CUICard className="card-brdr-no-shadow crd-img-radius">
-            <div>
+          <CUICard style={{ height: "239px" }} className="card-brdr-no-shadow crd-img-radius">
+            <div className="pos-rel">
               <img style={{ width: "100%", marginBottom: "-20px" }} src="/assets/LoginScreen/landing.png" alt="landing" />
-              <span className="pos-rel text--white" style={{ top: "-180px", left: "20px" }}>
+              <span className="pos-abs text--white txt-font" style={{ top: "50px", left: "50px" }}>
                 Electus DAICO
               </span>
-              <span className="pos-rel text--white" style={{ top: "-150px", left: "-98px" }}>
+              <span className="pos-abs text--white" style={{ top: "80px", left: "50px" }}>
                 Watch this space for news about Electus Daico
               </span>
-              <span className="pos-rel text--white" style={{ top: "-110px", left: "-478px" }}>
+              <span className="pos-abs text--white" style={{ top: "115px", left: "50px" }}>
                 <LoadingButton onClick={this.onWhiteListClick}>Get WhiteListed</LoadingButton>
               </span>
             </div>
           </CUICard>
         </div>
         <MasonryLayout>
-          <CUICard className="card-brdr-no-shadow crd-img-radius">
-            <div>
+          <CUICard style={{ height: "301px" }} className="card-brdr-no-shadow crd-img-radius">
+            <div className="pos-rel">
               <a href="https://storage.googleapis.com/electus/VaultProductDocument.pdf" rel="noreferrer noopener" target="_blank">
                 <img style={{ width: "100%", marginBottom: "-25px" }} src="/assets/LoginScreen/landing-1.png" alt="landing-1" />
               </a>
-              <span className="pos-rel text--white txt-font" style={{ top: "-220px", left: "20px" }}>
+              <span className="pos-abs text--white txt-font" style={{ top: "50px", left: "50px" }}>
                 Read Our Productpaper
               </span>
-              <span className="pos-rel text--white" style={{ top: "-200px", left: "-280px", textOverflow: "ellipsis" }}>
-                Vault is an accountable
-              </span>
-              <span className="pos-rel text--white" style={{ top: "-160px", left: "-465px", textOverflow: "ellipsis" }}>
-                crowdfunding platform
+              <span className="pos-abs text--white txt-m" style={{ top: "85px", left: "50px", textOverflow: "ellipsis" }}>
+                Vault is an accountable <br /> crowdfunding platform
               </span>
             </div>
           </CUICard>
 
-          <CUICard className="card-brdr-no-shadow crd-img-radius">
-            <div>
+          <CUICard style={{ height: "414px" }} className="card-brdr-no-shadow crd-img-radius">
+            <div className="pos-rel">
               <a
                 href="https://medium.com/@ParthaB/multi-layered-raises-and-why-they-are-relevant-c3e9369cda4f"
                 rel="noreferrer noopener"
@@ -57,23 +54,20 @@ class LandingPage extends Component {
               >
                 <img style={{ width: "100%" }} src="/assets/LoginScreen/landing-2.png" alt="landing-2" />
               </a>
-              <span className="pos-rel text--white txt-font" style={{ top: "-220px", left: "20px" }}>
+              <span className="pos-abs text--white txt-font" style={{ top: "50px", left: "50px" }}>
                 Why multi-layered fundraisers?
               </span>
-              <span className="pos-rel text--white txt-font" style={{ top: "-220px", left: "20px" }}>
-                To start with, unregulated Icos are
-              </span>
-              <span className="pos-rel text--white txt-font" style={{ top: "-220px", left: "20px" }}>
-                problematic
+              <span className="pos-abs text--white txt-m" style={{ top: "85px", left: "50px" }}>
+                To start with, unregulated Icos are <br /> problematic
               </span>
             </div>
           </CUICard>
-          <CUICard className="card-brdr-no-shadow crd-img-radius">
-            <div>
+          <CUICard style={{ height: "370px" }} className="card-brdr-no-shadow crd-img-radius">
+            <div className="pos-rel">
               <a href="https://storage.googleapis.com/electus/whitepaper.pdf" rel="noreferrer noopener" target="_blank">
                 <img style={{ width: "100%" }} src="/assets/LoginScreen/landing-3.png" alt="landing-3" />
               </a>
-              <span className="pos-rel text--white txt-font" style={{ top: "-220px", left: "20px" }}>
+              <span className="pos-abs text--white txt-font" style={{ top: "50px", left: "50px" }}>
                 Read about Electus Protocol
               </span>
             </div>

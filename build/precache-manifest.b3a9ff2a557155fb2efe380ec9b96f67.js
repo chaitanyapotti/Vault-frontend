cc53f360883b9a894bb8c3cd8c1fce9a24f872f5
chaitanya potti <chaitanya.potti@gmail.com>
2018-12-05 16:48:46 +0530

    created build

---
+++ build/precache-manifest.b3a9ff2a557155fb2efe380ec9b96f67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a4ac979527bf08393a3d",
+    "url": "/static/js/main.a4ac9795.chunk.js"
+  },
+  {
+    "revision": "6e6efbd166dd6eec96c6",
+    "url": "/static/js/1.6e6efbd1.chunk.js"
+  },
+  {
+    "revision": "a4ac979527bf08393a3d",
+    "url": "/static/css/main.52921559.chunk.css"
+  },
+  {
+    "revision": "6e6efbd166dd6eec96c6",
+    "url": "/static/css/1.92c4f8d5.chunk.css"
+  },
+  {
+    "revision": "6eb4f08ea19167bc5db7a581fda9985e",
+    "url": "/index.html"
+  }
+];

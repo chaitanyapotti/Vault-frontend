c9e73168028312a6c91176fc8e8a9c482cc26b00
chaitanya potti <chaitanya.potti@gmail.com>
2018-12-14 12:32:33 +0530

    bug fixes

--- src/actions/userTokensActions/index.js
+++ src/actions/userTokensActions/index.js
@@ -8,11 +8,12 @@ const version = config.vault_version;
 
 export const getUserTokens = userAddress => async dispatch => {
   const network = await web3.eth.net.getNetworkType();
+  const localVersion = version[network];
   // const network = "rinkeby";
   // await web3.eth.net.getNetworkType();
   axios
     .get(`${config.api_base_url}/projectweb3/tokens`, {
-      params: { network, version, address: userAddress }
+      params: { network, version: localVersion, address: userAddress }
     })
     .then(response => {
       if (response.status === 200) {
--- src/config.js
+++ src/config.js
@@ -6,6 +6,6 @@ module.exports = {
     main: "0x4ccD9f0cd3298a42645c266FEf90a728D122Ca6c",
     rinkeby: "0xAf32C5B541A5F62479Ad53531CE1a5Dbe4A73A5B"
   },
-  vault_version: {main: "1", rinkeby: "1"},
+  vault_version: { main: "1", rinkeby: "1" },
   poll_deployer_contract_address: "0x68e26B71c227db65C0886BA10BeC5f1A23Cae7e0"
 };

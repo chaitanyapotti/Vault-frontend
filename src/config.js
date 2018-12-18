2126086e467087f01ab8b61c2866838d1376b3b6
chaitanya potti <chaitanya.potti@gmail.com>
2018-12-18 22:32:31 +0530

    config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  api_base_url: "http://localhost:2020",
-  // api_base_url: "https://api.vault.electus.network",
+  // api_base_url: "http://localhost:2020",
+  api_base_url: "https://api.vault.electus.network",
   // vault_contract_address: "0xAf32C5B541A5F62479Ad53531CE1a5Dbe4A73A5B",
   vault_contract_address: {
     main: "0x4ccD9f0cd3298a42645c266FEf90a728D122Ca6c",

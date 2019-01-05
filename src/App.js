f1565588e63fb4eb922738cae1a3cd2462fe0455
chaitanyapotti <chaitanya@electus.network>
2019-01-05 11:09:47 +0530

    made app a pure component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /* eslint react/require-default-props: 0 */
 /* eslint camelcase: 0 */
 
-import React from "react";
+import React, { PureComponent } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 import "./static/css/app.css";
@@ -22,36 +22,38 @@ import LandingPage from "./pages/LandingPage";
 
 window.__MUI_USE_NEXT_TYPOGRAPHY_VARIANTS__ = true;
 
-const App = props => {
-  const { history } = props || {};
-  return (
-    <Provider store={store}>
-      <Router>
-        <VaultApp dispatch={store.dispatch} getState={store.getState} history={history}>
-          <Switch>
-            <Route exact path="/" component={LandingPage} history={history} />
-            <Route exact path="/mytokens" component={Governance} />
-            <Route strict path="/governance/details" component={ProjectGovernance} history={history} />
-            <Route strict path="/issuergovernance/details" component={ProjectIssuerGovernance} history={history} />
-            <Route exact path="/projects" component={AllProjects} />
-            <Route exact path="/registration" component={Registration} history={history} />
-            <Route strict path="/deploy" component={Deployer} history={history} />
-            <Route exact path="/register" component={WhiteList} />
-            {/* <Route exact path="/register" component={Register} /> */}
-            <Route strict path="/search" component={Search} />
-            <Route
-              path="/pollscan"
-              component={() => {
-                const searchPath = window.location.search;
-                window.location = `https://pollscan.io/contract${searchPath}`;
-              }}
-            />
-          </Switch>
-          <SigninManager />
-        </VaultApp>
-      </Router>
-    </Provider>
-  );
-};
+class App extends PureComponent {
+  render() {
+    const { history } = this.props || {};
+    return (
+      <Provider store={store}>
+        <Router>
+          <VaultApp dispatch={store.dispatch} getState={store.getState} history={history}>
+            <Switch>
+              <Route exact path="/" component={LandingPage} history={history} />
+              <Route exact path="/mytokens" component={Governance} />
+              <Route strict path="/governance/details" component={ProjectGovernance} history={history} />
+              <Route strict path="/issuergovernance/details" component={ProjectIssuerGovernance} history={history} />
+              <Route exact path="/projects" component={AllProjects} />
+              <Route exact path="/registration" component={Registration} history={history} />
+              <Route strict path="/deploy" component={Deployer} history={history} />
+              <Route exact path="/register" component={WhiteList} />
+              {/* <Route exact path="/register" component={Register} /> */}
+              <Route strict path="/search" component={Search} />
+              <Route
+                path="/pollscan"
+                component={() => {
+                  const searchPath = window.location.search;
+                  window.location = `https://pollscan.io/contract${searchPath}`;
+                }}
+              />
+            </Switch>
+            <SigninManager />
+          </VaultApp>
+        </Router>
+      </Provider>
+    );
+  }
+}
 
 export default App;
